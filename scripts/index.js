2a26c00589e594cee54082fa7c9e57d9002b255b
Evgenij Trofimov <evgenijtrofimov1302@gmail.com>
2025-06-03 12:50:13 +0300

    five project vs 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,33 @@
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+const cardTemplate = document.querySelector('#card-template').content;
+function deleteCard(cardElement) {
+    const placeList = document.querySelector('.places__list');
+    placeList.removeChild(cardElement);
+}
+function createCard(cardData, onDelete) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
-// @todo: Функция удаления карточки
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    cardTitle.textContent = cardData.name;
+
+    deleteButton.addEventListener('click', () => {
+        onDelete(cardElement);
+    });
+    return cardElement;
+}
+
+const placeList = document.querySelector('.places__list');
+
+initialCards.forEach((cardData) => {
+    const card = createCard(cardData, deleteCard)
+    placeList.appendChild(card);
+});
 
 // @todo: Вывести карточки на страницу
