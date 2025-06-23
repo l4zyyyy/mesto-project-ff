d88f81262d770c880c9e62b1fded876b6c8ae873
l4zyyyy <evgenijtrofimov1302@yandex.ru>
2025-06-24 00:43:09 +0300

    Project 5 1.3

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -24,34 +24,5 @@ const initialCards = [
       link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
     }
 ];
-export {initialCards}
 
-// components/card.js
-
-export function handleLikeClick(likeButton) {
-  likeButton.classList.toggle('card__like-button-liked');
-}
-
-export function deleteCard(cardElement) {
-  cardElement.remove();
-}
-
-export function createCard(cardData, onDelete, onLike, onImageClick) {
-  const cardTemplate = document.querySelector('#card-template').content;
-  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-
-  const cardImage = cardElement.querySelector('.card__image');
-  const cardTitle = cardElement.querySelector('.card__title');
-  const deleteButton = cardElement.querySelector('.card__delete-button');
-  const likeButton = cardElement.querySelector('.card__like-button');
-
-  cardImage.src = cardData.link;
-  cardImage.alt = cardData.name;
-  cardTitle.textContent = cardData.name;
-
-  deleteButton.addEventListener('click', () => onDelete(cardElement));
-  likeButton.addEventListener('click', () => onLike(likeButton));
-  cardImage.addEventListener('click', () => onImageClick(cardData));
-
-  return cardElement;
-}
+export { initialCards };
